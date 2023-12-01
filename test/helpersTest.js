2c693ce9f8c03a70f03d2c3a7ac4c27b47acc6b8
Jonathan Lao <jon.lao@gmail.com>
2023-11-30 20:04:32 -0600

    added mocha+chai, created helpersTest

---
+++ test/helpersTest.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', () => {
+    const user = getUserByEmail("user@example.com", testUsers)
+    // const expectedUserID = "userRandomID";
+    assert.equal(user, testUsers.userRandomID);
+  });
+
+  it('should return undefined when looking for a non-existent email', () => {
+    const user = getUserByEmail('someuser@example.com', testUsers);
+    assert.equal(user, undefined);
+  });
+});
